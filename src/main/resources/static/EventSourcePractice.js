11337aff2c94542211db10146cd7ab23a480bd06
davidalk <david.alkanani@gmail.com>
2015-12-31 20:33:31 +0000

    amend js

--- src/main/resources/static/EventSourcePractice.js
+++ src/main/resources/static/EventSourcePractice.js
@@ -1,10 +1,10 @@
-"use strict";
+'use strict';
 
 $(document).ready(function () {
     var $messageContainer = $('#messageContainer');
 
     //noinspection JSUnresolvedFunction
-    var evtSource = new EventSource("/sse");
+    var evtSource = new EventSource('/sse');
 
     evtSource.onmessage = function (e) {
         $messageContainer.html(e.data);

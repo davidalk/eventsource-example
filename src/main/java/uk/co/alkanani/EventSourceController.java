bf64b7070c0141d5c143a3cbf0722c0c5adecd59
davidalk <david.alkanani@gmail.com>
2015-12-31 20:01:59 +0000

    setup html

--- src/main/java/uk/co/alkanani/EventSourceController.java
+++ src/main/java/uk/co/alkanani/EventSourceController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
